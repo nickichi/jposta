b4aecc0ae070c60ad7c5d36061d05851c6e1eaab
taichika <10208849+nickichi@users.noreply.github.com>
2024-06-14 20:36:44 +0900

    init

---
+++ lib/jposta.ts
@@ -0,0 +1,121 @@
+type JpostaConfig = {
+	host: string;
+};
+
+const defaultConfig: JpostaConfig = {
+	host: "",
+};
+
+const currentConfig = defaultConfig;
+
+const prefs = [
+	"北海道",
+	"青森県",
+	"岩手県",
+	"宮城県",
+	"秋田県",
+	"山形県",
+	"福島県",
+	"茨城県",
+	"栃木県",
+	"群馬県",
+	"埼玉県",
+	"千葉県",
+	"東京都",
+	"神奈川県",
+	"新潟県",
+	"富山県",
+	"石川県",
+	"福井県",
+	"山梨県",
+	"長野県",
+	"岐阜県",
+	"静岡県",
+	"愛知県",
+	"三重県",
+	"滋賀県",
+	"京都府",
+	"大阪府",
+	"兵庫県",
+	"奈良県",
+	"和歌山県",
+	"鳥取県",
+	"島根県",
+	"岡山県",
+	"広島県",
+	"山口県",
+	"徳島県",
+	"香川県",
+	"愛媛県",
+	"高知県",
+	"福岡県",
+	"佐賀県",
+	"長崎県",
+	"熊本県",
+	"大分県",
+	"宮崎県",
+	"鹿児島県",
+	"沖縄県",
+];
+
+export type Address = {
+	pref: string;
+	prefNum: number;
+	city: string;
+	town?: string;
+};
+
+export const getAddress = async (zipCode: string): Promise<Address | null> => {
+	if (zipCode.length !== 7 && zipCode.length !== 8) {
+		throw new Error(`Zip code must be 7 or 8 characters: ${zipCode}`);
+	}
+	if (zipCode.length === 8 && !/^\d{3}-\d{4}$/.test(zipCode)) {
+		throw new Error(`Invalid zip code: ${zipCode}`);
+	}
+	if (zipCode.length === 7 && !/^\d{7}$/.test(zipCode)) {
+		throw new Error(`Invalid zip code: ${zipCode}`);
+	}
+
+	const zip = zipCode.replace("-", "");
+	const area = zip.slice(0, 2);
+	const json = await fetchJson(area);
+
+	if (!json || !json[zip]) {
+		return null;
+	}
+
+	const [prefNum, city, town] = json[zip];
+	const pref = prefs[prefNum - 1];
+
+	if (
+		typeof prefNum !== "number" ||
+		typeof city !== "string" ||
+		typeof town !== "string" ||
+		typeof pref !== "string"
+	) {
+		throw new Error(`Internal error data broken: ${json[zip]}`);
+	}
+
+	return {
+		pref: pref,
+		prefNum: prefNum,
+		city: city,
+		town: town || undefined,
+	};
+};
+
+const fetchJson = async (chunk: string) => {
+	if (currentConfig.host !== "") {
+		const { default: json } = await import(
+			`${currentConfig.host}/z${chunk}.json`
+		);
+		return json;
+	}
+
+	const { default: json } = await import(`./zips/z${chunk}.json`);
+	return json;
+};
+
+export const configureJposta = (config: Partial<JpostaConfig>) => {
+	currentConfig.host = config.host || defaultConfig.host;
+};
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  plugins: [dts()],
+  build: {
+    lib: {
+      entry: './lib/index.ts',
+      name: 'jposta',
+      fileName: 'index',
+      formats: ['es']
+    }
+  }
+})
